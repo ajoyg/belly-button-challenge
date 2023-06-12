671f36bf6a3726f520fa28f7d9f737699e8b9263
ajoyg <ajay.gopalkrishna@gmail.com>
2023-06-12 16:57:03 -0700

    Added README and the hovertext for the bar chart in app.js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# belly-button-challenge
-UCB Module 14  D3.js challenge
+# Belly Button Challenge
+This challenge is a part of the UC Berkeley Data Analytics and Visualization Bootcamp Module 14 challenge. The assignment is to build an interactive dashboard to explore the ![Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
+
+The interactive dashboard read from from a json file. The app.js in the static folder creates the follwing elements of the dashboard:
+1. A drop down list of subject IDs that were studied as part of the analysis.
+2. A bar chart of the top 10 microbial species or operational taxonomic units (OTU) for each subject.
+3. A bubble chart of all the different OTU values for each subject.
+4. A gauge chart that shows how frequently the subject washed their belly botton per week.
+5. The dashboard is updated every time the use picks a new subject.
+
+The interactive dashboard is live ![here](https://ajoyg.github.io/belly-button-challenge/).
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,9 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-
+// Get the data from the samples.json file
 d3.json(url).then(function(data) {
   let allIds = data.names;
-  console.log(allIds);
+  // Populate the list of samples
   d3.select("#selDataset")
       .selectAll('myOptions')
      	.data(allIds)
@@ -13,6 +13,7 @@ d3.json(url).then(function(data) {
       .attr("value", function (d) { return d; }) // corresponding value returned by the button
 
   let nameID = d3.select("#selDataset").property("value");
+  //Draw the charts on the page
   drawAllCharts(nameID);
   });
   
@@ -20,7 +21,8 @@ d3.json(url).then(function(data) {
 function drawAllCharts(nameID)
 {
     d3.json(url).then(function(data){
-    
+
+    // Show the details of the sample
     let sampleMetadata = data.metadata.filter(sample => sample.id == nameID)[0];
     d3.select(".panel-body").html("");
     for (const[key,value] of Object.entries(sampleMetadata)) 
@@ -28,30 +30,33 @@ function drawAllCharts(nameID)
       d3.select(".panel-body").append("small").text(key+": "+value).append("br");
     };
     
+    // Get the data for the specific sample
     let individualSamples = data.samples.filter(sample => sample.id == nameID)[0];
-    console.log(individualSamples);
-  
-    let top10SortedSampleValues = individualSamples['sample_values'].slice(0, 10);
-    console.log(top10SortedSampleValues);
     
+    // Get the top 10 values
+    let top10SortedSampleValues = individualSamples['sample_values'].slice(0, 10);
     let top10OTUIds = individualSamples['otu_ids'].slice(0,10);
-    console.log(top10OTUIds);
     let top10OTUValues = top10OTUIds.map(otuId => "OTU " + otuId);
-    console.log(top10OTUValues);
+    let top10OTULabels = individualSamples['otu_labels'].slice(0,10);
+    
+    //Reverse the arrays for the bar graph
     let reverseSortedSampleValues = top10SortedSampleValues.reverse();
     let reverseSortedOTUValues = top10OTUValues.reverse();
+    let reverseSortedOTULabels = top10OTULabels.reverse();
 
+    //Plot the bar graph
     let trace1 = {
        x: reverseSortedSampleValues, 
        y: reverseSortedOTUValues,
        text: top10OTUValues,
       type: 'bar',
-      orientation: 'h'
+      orientation: 'h',
+      hovertext: reverseSortedOTULabels
      }
      let plotData =[trace1];
      Plotly.newPlot('bar', plotData);
 
-
+    //Get the data for the gauge chart
      let wScrubData = [
       {
         type: "indicator",
@@ -66,10 +71,10 @@ function drawAllCharts(nameID)
           borderwidth: 2,
           bordercolor: "gray",
           steps: [
-            { range: [0,1], color: "rgba(228, 242, 207, .5)", name : "0-1", visible: true },
-            { range: [1,2], color: "rgba(210,233, 175, .5)" , name : "1-2", visible: true },
-            { range: [2,3], color: "rgba(192, 224, 143, .5)" , name : "2-3", visible: true },
-            { range: [3,4], color: "rgba(174, 216, 111, .5)", name: "3-4", visible: true },
+            { range: [0,1], color: "rgba(228, 242, 207, .5)" },
+            { range: [1,2], color: "rgba(210,233, 175, .5)"  },
+            { range: [2,3], color: "rgba(192, 224, 143, .5)" },
+            { range: [3,4], color: "rgba(174, 216, 111, .5)" },
             { range: [4,5], color: "rgba(156, 207, 53, .5)" },
             { range: [5,6], color: "rgba(136, 192, 22, .5)" },
             { range: [6,7], color: "rgba(114, 160, 44, .5)" },
@@ -85,9 +90,9 @@ function drawAllCharts(nameID)
         }
     } ];
     
-  
    Plotly.newPlot('gauge', wScrubData);
     
+   //Plot the bubble chart for all the OTU values
     var layout = {
       width: 900,
       height: 500,
@@ -95,7 +100,6 @@ function drawAllCharts(nameID)
 
     };
     
-
     let trace2= {
       x: individualSamples['otu_ids'],
       y: individualSamples['sample_values'],
